d271c5aff550c395b583f4567da62c9cf9acb313
forever <806051642@qq.com>
2020-05-15 17:07:37 +0800

    页面修改

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,27 @@
  * Created by Administrator on 2020/5/8.
  */
 $(function(){
+    //导航菜单
+    var ind = 4;
+    var nav= jQuery(".nav");
+    // var init = jQuery(".nav .m").eq(ind);
+    // var block = jQuery(".nav .block");
+    // block.css({"left":init.position().left-3});
+    // nav.hover(function(){},function(){ block.animate({"left":init.position().left-3},100); });
+
+    jQuery(".nav").slide({
+        type:"menu",
+        titCell:".m",
+        targetCell:".sub",
+        delayTime:300,
+        triggerTime:0,
+        returnDefault:true,
+        defaultIndex:ind
+        // startFun:function(i,c,s,tit){
+        //     block.animate({"left":tit.eq(i).position().left-3},100);
+        // }
+    });
+
     //banner
     jQuery(".full_banner").slide({
         titCell:".hd ul",
@@ -12,4 +33,7 @@ $(function(){
         trigger:"click",
         interTime:3500
     });
+
+
+
 });
---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2020/5/15.
+ */
+$(function(){
+    //导航菜单
+    var admin= jQuery(".admin");
+    // var init = jQuery(".nav .m").eq(ind);
+    // var block = jQuery(".nav .block");
+    // block.css({"left":init.position().left-3});
+    // nav.hover(function(){},function(){ block.animate({"left":init.position().left-3},100); });
+
+});
